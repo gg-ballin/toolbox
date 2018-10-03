73674bdddcf1e695691f5515a9c09dc138c361ed
unknown <gomezger.a9@gmail.com>
2018-10-03 19:55:21 -0300

    sin node_m

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,4 +33,4 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-export default connect (mapDispatchToProps,mapStateToProps)(App);
+export default connect (mapStateToProps,mapDispatchToProps)(App);
